2437e7968fafea6c67442a764a5f1ed9dfc67a6f
Shahan Sikandar <124002166+shahan-sikandar@users.noreply.github.com>
2026-01-22 23:18:30 +0500

    Update medibot.py

--- medibot.py
+++ medibot.py
@@ -106,8 +106,7 @@ import os
 import streamlit as st
 
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
@@ -254,9 +253,8 @@ def main():
             # Use hybrid retriever to include PDFs + CSVs
             with st.spinner("🔍 Analyzing medical knowledge..."):
                 docs = hybrid_retriever(vectorstore, prompt, k_value)
-                response = qa_chain.invoke({
-                                "input": prompt
-                                })
+                response = qa_chain.run(prompt)
+
 
 
 
@@ -282,4 +280,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
